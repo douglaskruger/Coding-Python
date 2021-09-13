737f3b7adf849679eba988bfd029cf2c11cd7289
Douglas Kruger <douglas.d.kruger@gmail.com>
2021-09-14 10:58:06 +1200

    Add files via upload
    Initial version

---
+++ python-web/get_webpage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# *******************************************************
+# https://docs.aiohttp.org/en/stable/client_reference.html
+# *******************************************************
+import aiohttp
+import asyncio
+
+async def fetch(client):
+    async with client.get('http://python.org') as resp:
+        assert resp.status == 200
+        return await resp.text()
+
+async def main():
+    async with aiohttp.ClientSession() as client:
+        html = await fetch(client)
+        print(html)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
